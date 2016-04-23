d6d8c98df675c42a81c79a059a6e43fdcce78660
eric <halstead.eric@gmail.com>
2016-04-23 15:47:11 -0700

    page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Guatemala Silent Auction"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Guatemala Silent Auction"
   end
 
   test "should get donate" do
     get :donate
     assert_response :success
+    assert_select "title", "Donate | Guatemala Silent Auction"
   end
 
 end
