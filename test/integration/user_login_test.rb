e204cbde38f7a87431c1f5ee28119b89581a4508
eric <halstead.eric@gmail.com>
2017-02-18 10:08:15 -0800

    login with session cookies

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -15,11 +15,13 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.present?
   end
 
-  test "login with valid credentials" do
+  test "login and logout with valid credentials" do
+    # login
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
     assert is_logged_in?
 
+    # check to make sure we get the logged in view
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -27,9 +29,17 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
 
+    # logout
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+
+    # simulate logging out a second time from a concurrent session
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
