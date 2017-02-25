58a0ece150371659874f19535f90ae81d17d7da7
eric <halstead.eric@gmail.com>
2017-02-25 15:20:58 -0800

    redirect_to rather than render on update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile updated!'
-      render @user
+      redirect_to @user
     else
       render 'edit'
     end
