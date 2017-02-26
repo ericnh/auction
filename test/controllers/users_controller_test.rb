4dd1a6aca36dac49288c3116a5b3fb17fde75a19
eric <halstead.eric@gmail.com>
2017-02-25 17:16:03 -0800

    test logged_in_user authorization check

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:bob)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
     assert_select "title", "Sign Up | Guatemala Silent Auction"
+  end
+
+  test "should redirect away from edit when not logged in" do
+    get :edit, id: @user.id
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect away from update when not logged in" do
+    patch :update, id: @user.id, user: {name: @user.name, email: @user.email}
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
